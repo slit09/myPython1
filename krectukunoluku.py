e1dc9572fade1f9a3ce87ad8a1c9496b2e6e1449
slit09 <maryva@mail.ru>
2022-09-24 13:36:01 +0300

    бебра

--- krectukunoluku.py
+++ krectukunoluku.py
@@ -4,8 +4,10 @@
 #раздел импортных модулей
 #----------------------------------------------
 from ast import Return
+from os import POSIX_FADV_SEQUENTIAL, posix_spawn
 import random
 from re import X
+import re
 #----------------------------------------------
 #раздел созданных функций
 #----------------------------------------------
@@ -35,14 +37,60 @@ def whoGoesfirs():
     else:
         return 'микрочелл' 
 
-def golginwey(board,letter,move):
-    board[letter] = move
+def makeMove(board,letter,move):
+    board[move] = letter
+
+def golgirwer(bo,it):
+
+    return((bo[7]==it and bo[8]==it and bo[9]==it) or
+       (bo[4]==it and bo[5]==it and bo[6]==it) or
+       (bo[1]==it and bo[2]==it and bo[3]==it) or
+       (bo[7]==it and bo[4]==it and bo[1]==it) or
+       (bo[8]==it and bo[5]==it and bo[2]==it) or 
+       (bo[9]==it and bo[6]==it and bo[3]==it) or
+       (bo[7]==it and bo[5]==it and bo[3]==it) or
+       (bo[9]==it and bo[5]==it and bo[1]==it))
+def getBoardCopy(board):
+    BoardCopy = []
+    for i in board:
+        BoardCopy.append(i)
+    return BoardCopy
+
+def isSpaceFree(board,move):
+    return board[move] == ' '
+
+def getPlayerMove(board):
+    move = ' '
+    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board,int(move)):
+          print('Ваш следующий ход? Введите номер ячейки.(1-9)')
+          move = input()
+          return int(move)
+          
+def cholrasrtMoveFRomlist(board,movesList):
+    possibleMoves = []
+    for i in movesList:
+        if isSpaceFree(board,i):
+            possibleMoves.append(i)
+
+            if len(possibleMoves) != 0:
+                return random.choice(possibleMoves)
+            else:   
+                return None
+         
 #----------------------------------------------
 #основное тело программы
 #----------------------------------------------
-board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+board = [' ']*10
+ml = [1,3,7,9]
 displayBoard(board)
-board[1] = 'O'
-board[5] = 'X'
-board[9] = 'O'
+hod = cholrasrtMoveFRomlist(board,ml)
+#
 displayBoard(board)
+
+board[1] = 'X'
+board[7] = 'X'
+board[9] = 'X'
+hod = cholrasrtMoveFRomlist(board,ml)
+#
+displayBoard(board)
+
--- test2.py
+++
@@ -1,7 +0,0 @@
-from operator import truediv
-
-
-def primer(p1,p2,p3,p4,p5,p6):
-    return (p1 and p2 and p3) or (p4 and p5 and p6)
-
-print(primer(True,False,True,True,False,True))
